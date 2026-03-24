90bf6728cdbd38ecb92e68110058c6dc9cd23d17
hxuvsck <hxuvsck@gmail.com>
2026-03-24 22:12:25 +0800

    Checking the fetch of PokeAPI

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+
+  useEffect(() => {
+    // fetch pokemons
+    fetchPokemons();
+  }, [])
+
+  async function fetchPokemons() {
+    try {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=20");
+      const data = await response.json();
+
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  useState
+
   return (
     <View
       style={{
