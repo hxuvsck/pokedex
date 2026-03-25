5aec3ad4150e78b6284a2061cbd002535fd0f695
hxuvsck <hxuvsck@gmail.com>
2026-03-25 12:38:20 +0800

    DetailedPokemon async func added with switching url to image

--- app/index.tsx
+++ app/index.tsx
@@ -3,10 +3,10 @@ import { ScrollView, Text, View } from "react-native";
 
 interface Pokemon {
   name: string;
-  url: string;
+  image: string;
 }
 
-export default function Index() {
+export default async function Index() {
 
   // pokemons var will list the data of API fetch that is from setPokemons
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
@@ -18,10 +18,22 @@ export default function Index() {
 
   async function fetchPokemons() {
     try {
-      const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=20");
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=10");
       const data = await response.json();
 
-      setPokemons(data.results);
+      // Fetch detailed info for each Pokemon in parallel
+      const detailedPokemons = await Promise.all(
+        data.results.map(async (pokemon: Pokemon) => {
+          const res = await fetch(pokemon.url);
+          const details = await res.json();
+          return {
+            name: pokemon.name,
+            image: details.sprites.front_default, //main sprite 
+          };
+        })
+      );
+
+      setPokemons(detailedPokemons);
     } catch (e) {
       console.log(e);
     }
