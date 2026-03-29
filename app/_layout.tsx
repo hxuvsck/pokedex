2cda4f2cd16d7fd3dadb401916d4a0c916c4fc2a
hxuvsck <hxuvsck@gmail.com>
2026-03-29 13:03:26 +0800

    Center align the header texts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,7 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack>
+  return <Stack screenOptions={{ headerTitleAlign: "center" }}>
     <Stack.Screen name="index" options={{ title: "Home" }} />
     <Stack.Screen name="details" options={{ title: "Details", headerBackButtonDisplayMode: "minimal" }} />
   </Stack>;
