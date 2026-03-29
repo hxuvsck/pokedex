6137ab73e6cbbb796b1b79f5217e1eb8474bec18
hxuvsck <hxuvsck@gmail.com>
2026-03-29 12:32:21 +0800

    chore: Initialize details file

---
+++ app/details.tsx
@@ -0,0 +1,17 @@
+import { ScrollView, StyleSheet } from "react-native";
+
+export default async function Details() {
+
+    return (
+        <ScrollView
+            contentContainerStyle={{
+                gap: 16,
+                padding: 16,
+            }}>
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
